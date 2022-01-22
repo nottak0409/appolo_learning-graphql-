b40257f36f573d08a64780ccfba81e44ea1bb125
nottak0409 <nottak0409@gmail.com>
2022-01-22 20:40:57 +0900

    schema定義

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -1,7 +1,41 @@
 const { gql } = require("apollo-server");
 
 const typeDefs = gql`
-  # ここにスキーマを書きます
+    type Query {
+        launches: [Launch]!
+        launch(id: ID!): Launch
+        me: User
+    }
+
+    type Launch {
+        id: ID!
+        site: String
+        mission: Mission
+        rocket: Rocket
+        isBooked: Boolean!
+    }
+
+    type Rocket {
+        id: ID!
+        name: String
+        type: String
+    }
+
+    type User {
+        id: ID!
+        email: String!
+        trips: [Launch]!
+    }
+
+    type Mission {
+        name: String
+        missionPatch(size: PatchSize): String
+    }
+
+    enum PatchSize {
+        SMALL
+        LARGE
+    }
 `;
 
 module.exports = typeDefs;
