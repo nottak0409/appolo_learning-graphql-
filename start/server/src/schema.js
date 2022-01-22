8c319a6d29f36adbd175bd02f8eef395bec49cf6
nottak0409 <nottak0409@gmail.com>
2022-01-23 08:50:18 +0900

    schema定義追加

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -1,51 +1,56 @@
 const { gql } = require("apollo-server");
 
 const typeDefs = gql`
-    type Mutation {
-        # if false, booking trips failed --check errors
-        bookTrips(launchIds: [ID]!): TripUpdateResponse!
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
+  type Mutation {
+    # if false, booking trips failed --check errors
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
 
-        # if false, cancellation failed --check errors
-        cancelTrip(launchId: ID!): TripUpdateResponse!
+    # if false, cancellation failed --check errors
+    cancelTrip(launchId: ID!): TripUpdateResponse!
 
-        login(email: String): String # login token
-    }
+    login(email: String): String # login token
+  }
 
-    type Query {
-        launches: [Launch]!
-        launch(id: ID!): Launch
-        me: User
-    }
+  type Query {
+    launches: [Launch]!
+    launch(id: ID!): Launch
+    me: User
+  }
 
-    type Launch {
-        id: ID!
-        site: String
-        mission: Mission
-        rocket: Rocket
-        isBooked: Boolean!
-    }
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
 
-    type Rocket {
-        id: ID!
-        name: String
-        type: String
-    }
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
 
-    type User {
-        id: ID!
-        email: String!
-        trips: [Launch]!
-    }
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+  }
 
-    type Mission {
-        name: String
-        missionPatch(size: PatchSize): String
-    }
+  type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
+  }
 
-    enum PatchSize {
-        SMALL
-        LARGE
-    }
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
 `;
 
 module.exports = typeDefs;
