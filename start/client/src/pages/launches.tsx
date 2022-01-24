ff498b440333487e325d1c46a0f5afbdae288aa6
nottak0409 <nottak0409@gmail.com>
2022-01-25 08:58:56 +0900

    launches作成

--- start/client/src/pages/launches.tsx
+++ start/client/src/pages/launches.tsx
@@ -1,6 +1,29 @@
 import React, { Fragment, useState }  from 'react';
 import { RouteComponentProps } from '@reach/router';
 import { gql } from '@apollo/client'
+import { LaunchTile, Header, Button, Loading } from '../components';
+import { useQuery } from '@apollo/react-hooks';
+
+const GET_LAUNCHES = gql`
+  query launchList($after: String) {
+    launches(after: $after) {
+      cursor
+      hasMore
+      launches {
+        id
+        isBooked
+        rocket {
+          id
+          name
+        }
+        mission {
+          name
+          missionPatch
+        }
+      }
+    }
+  }
+`;
 
 export const LAUNCH_TILE_DATA = gql`
   fragment LaunchTile on Launch {
@@ -20,8 +43,22 @@ export const LAUNCH_TILE_DATA = gql`
 
 interface LaunchesProps extends RouteComponentProps {}
 
-const Launches: React.FC<LaunchesProps> = () => {
-  return <div />;
-}
+export default function Launches() {
+  const { data, loading, error } = useQuery(GET_LAUNCHES);
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR</p>;
 
-export default Launches;
+  return (
+    <Fragment>
+      <Header />
+      {data.launches &&
+        data.launches.launches &&
+        data.launches.launches.map(launch => (
+          <LaunchTile
+            key={launch.id}
+            launch={launch}
+          />
+        ))}
+    </Fragment>
+  );
+}
