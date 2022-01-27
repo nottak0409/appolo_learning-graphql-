ae1f9dbea3b91d331e3f791ee4376ac6746f8fb6
nottak0409 <nottak0409@gmail.com>
2022-01-28 08:37:14 +0900

    launches作成

--- start/client/src/pages/launches.tsx
+++ start/client/src/pages/launches.tsx
@@ -1,29 +1,9 @@
-import React, { Fragment, useState }  from 'react';
-import { RouteComponentProps } from '@reach/router';
-import { gql } from '@apollo/client'
-import { LaunchTile, Header, Button, Loading } from '../components';
-import { useQuery } from '@apollo/react-hooks';
+import React, { Fragment, useState } from 'react';
+import { gql, useQuery } from '@apollo/client';
 
-const GET_LAUNCHES = gql`
-  query launchList($after: String) {
-    launches(after: $after) {
-      cursor
-      hasMore
-      launches {
-        id
-        isBooked
-        rocket {
-          id
-          name
-        }
-        mission {
-          name
-          missionPatch
-        }
-      }
-    }
-  }
-`;
+import { LaunchTile, Header, Button, Loading } from '../components';
+import { RouteComponentProps } from '@reach/router';
+import * as GetLaunchListTypes from './__generated__/GetLaunchList';
 
 export const LAUNCH_TILE_DATA = gql`
   fragment LaunchTile on Launch {
@@ -41,52 +21,63 @@ export const LAUNCH_TILE_DATA = gql`
   }
 `;
 
+export const GET_LAUNCHES = gql`
+  query GetLaunchList($after: String) {
+    launches(after: $after) {
+      cursor
+      hasMore
+      launches {
+        ...LaunchTile
+      }
+    }
+  }
+  ${LAUNCH_TILE_DATA}
+`;
+
 interface LaunchesProps extends RouteComponentProps {}
 
-export default function Launches() {
-   const { data, loading, error, fetchMore } = useQuery(GET_LAUNCHES);
+const Launches: React.FC<LaunchesProps> = () => {
+  const {
+    data,
+    loading,
+    error,
+    fetchMore
+  } = useQuery<
+    GetLaunchListTypes.GetLaunchList,
+    GetLaunchListTypes.GetLaunchListVariables
+  >(GET_LAUNCHES);
+  const [isLoadingMore, setIsLoadingMore] = useState(false);
+
   if (loading) return <Loading />;
-  if (error) return <p>ERROR</p>;
+  if (error || !data) return <p>ERROR</p>;
 
   return (
     <Fragment>
       <Header />
       {data.launches &&
         data.launches.launches &&
-        data.launches.launches.map(launch => (
-          <LaunchTile
-            key={launch.id}
-            launch={launch}
-          />
+        data.launches.launches.map((launch: any) => (
+          <LaunchTile key={launch.id} launch={launch} />
         ))}
-        {data.launches &&
-  data.launches.hasMore && (
-    <Button
-      onClick={() =>
-        fetchMore({
-          variables: {
-            after: data.launches.cursor,
-          },
-          updateQuery: (prev, { fetchMoreResult, ...rest }) => {
-            if (!fetchMoreResult) return prev;
-            return {
-              ...fetchMoreResult,
-              launches: {
-                ...fetchMoreResult.launches,
-                launches: [
-                  ...prev.launches.launches,
-                  ...fetchMoreResult.launches.launches,
-                ],
-              },
-            };
-          },
-        })
-      }
-    >
-      Load More
-    </Button>
-  )
-    }
+      {data.launches && data.launches.hasMore && (
+        isLoadingMore
+          ? <Loading />
+          : <Button
+              onClick={async () => {
+                setIsLoadingMore(true);
+                await fetchMore({
+                  variables: {
+                    after: data.launches.cursor,
+                  },
+                });
+                setIsLoadingMore(false);
+              }}
+            >
+              Load More
+            </Button>
+      )}
     </Fragment>
   );
 }
+
+export default Launches;
